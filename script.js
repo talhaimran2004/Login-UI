1078ea40e37723a8f154d8394a1b4b3af5005407
talhaimran2004 <79780727+talhaimran2004@users.noreply.github.com>
2021-08-31 16:34:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function register() {
+x.style.left = "-850px";
+y.style.left = "8px";
+z.style.left = "110px";
+}
+
+function login() {
+x.style.left = "8px";
+y.style.left = "500px";
+z.style.left = "0px";
+}
